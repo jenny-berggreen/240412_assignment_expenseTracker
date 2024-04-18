cc693767e51b1de77dc7841d962d48d73917d982
jenny-berggreen <jenny@berggreen.no>
2024-04-18 12:18:15 +0200

    Add expense list elements

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import styles from '../styles/style.module.css'
+import listStyles from '../styles/expenseList.module.css'
 
 const ExpenseList = () => {
   return (
-	<div>ExpenseList</div>
+	<div className={`${listStyles.expense_list_container}`}>
+		<span className={`${listStyles.expense_list_row} ${styles.flex} ${styles.flex_row}`}>
+			<div className={`${listStyles.title_date_container} ${listStyles.list_row_item__title_date}`}>
+				<span>dd/mm/yy</span>
+				<span>Rent April</span>
+			</div>
+			<div className={`${listStyles.category_container} ${listStyles.list_row_item__category} ${styles.flex}`}>
+				<span>Housing</span>
+			</div>
+			<div className={`${listStyles.amount_container} ${listStyles.list_row_item__amount} ${styles.flex}`}>
+				<span>$3.50</span>
+			</div>
+			<div className={`${listStyles.button_container} ${listStyles.list_row_item__button} ${styles.flex}`}>
+				<button className={`${listStyles.delete_button}`}>Delete</button>
+			</div>
+		</span>
+	</div>
   )
 }
 
