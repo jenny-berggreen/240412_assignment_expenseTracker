d3901417de228c69ab308aa89f4d6f56c9568d0e
jenny-berggreen <jenny@berggreen.no>
2024-04-18 11:55:22 +0200

    Style app elements

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,39 @@ function App() {
   return (
     <>
     <div className={`${appStyles.app_container} ${styles.flex} ${styles.flex_column}`}>
+      {/* NAVBAR */}
       <nav className={`${appStyles.navbar} ${styles.flex}`}>
         <span className={`${appStyles.nav_header}`}>Expense Tracker</span>
       </nav>
 
-      <div className={`${styles.flex} ${styles.flex_row}`}>
+      <div className={`${appStyles.main_container} ${styles.flex} ${styles.flex_row}`}>
+        {/* CATEGORIES */}
         <div className={`${appStyles.category_container} ${styles.flex} ${styles.flex_column}`}>
           <h2>Total category expenses</h2>
         </div>
+
+        <div className={`${appStyles.form_and_list_container} ${styles.grid}`}>
+          {/* TOTAL EXPENSES */}
+          <div className={`${appStyles.total_expenses_container}`}>
+            Total expenses container
+          </div>
+
+          {/* EXPENSE FORM */}
+          <div className={`${appStyles.expense_form_container} ${styles.flex} ${styles.flex_column}`}>
+            <h2>Add expense</h2>
+            <ExpenseForm />
+          </div>
+
+          {/* DISPLAY EXPENSES */}
+          <div className={`${appStyles.expense_list_container} ${styles.flex} ${styles.flex_column}`}>
+            <h2>All expenses</h2>
+            <span>list of expenses</span>
+          </div>
+        </div>
+
       </div>
 
-      <ExpenseForm />
+      
     </div>
     
       
