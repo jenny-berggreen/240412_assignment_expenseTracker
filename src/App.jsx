af1f7b4675a8d23aada075e23082f847a4e9ecb7
jenny-berggreen <jenny@berggreen.no>
2024-04-18 10:29:17 +0200

    Add form elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import './styles/style.module.css'
+import ExpenseForm from './components/ExpenseForm'
 
 function App() {
 
   return (
     <>
+    <ExpenseForm />
       
     </>
   )
--- src/components/expenseForm.jsx
+++ src/components/expenseForm.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import styles from '../styles/style.module.css'
+import formStyles from '../styles/expenseForm.module.css'
 
-const expenseForm = () => {
+const ExpenseForm = () => {
+
   return (
-	<div>expenseForm</div>
+    <>
+    <form className={`${formStyles.expense_form} ${styles.flex}`}>
+      <fieldset className={`${formStyles.expense_form_container} ${styles.flex} ${styles.flex_column}`}>
+
+        <section className={`${styles.flex} ${styles.flex_row}`}>
+          <div className={`${formStyles.input_group} ${styles.flex} ${styles.flex_column}`}>
+            <label htmlFor="title">Title<sup>*</sup></label>
+						<input type="text" name='title' placeholder='Ex. bus ticket' className={`${formStyles.input_element}`} />
+						<p>Error</p>
+          </div>
+          <div className={`${formStyles.input_group} ${styles.flex} ${styles.flex_column}`}>
+            <label htmlFor="amount">Amount<sup>*</sup></label>
+						<input type="text" name='amount' placeholder='Ex. 3.50' className={`${formStyles.input_element} ${formStyles.input_element__amount}`} />
+						<p>Error</p>
+          </div>
+        </section>
+
+        <div className={`${formStyles.input_group} ${styles.flex} ${styles.flex_column}`}>
+            <label htmlFor="date">Date<sup>*</sup></label>
+						<input type="date" name='date' className={`${formStyles.input_element}`} />
+						<p>Error</p>
+        </div>
+
+        <div className={`${formStyles.input_group} ${styles.flex} ${styles.flex_column}`}>
+            <label htmlFor="category">Category<sup>*</sup></label>
+						<select name="category">
+              <option value="noCategory">No category</option>
+              <option value="housing">Housing</option>
+              <option value="groceries">Groceries</option>
+              <option value="transportation">Transportation</option>
+              <option value="clothes">Clothes</option>
+              <option value="other">Other</option>
+            </select>
+						<p>Error</p>
+          </div>
+
+        <button className={`${formStyles.submit_button} ${styles.flex}`}>Add expense</button>
+
+      </fieldset>
+    </form>
+    
+    </>
+	
+
   )
 }
 
-export default expenseForm
+export default ExpenseForm
