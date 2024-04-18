5679ebf6f39045931657f9846436ed25ca215ca4
jenny-berggreen <jenny@berggreen.no>
2024-04-18 11:31:24 +0200

    Add and style App elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
-import './App.css'
-import './styles/style.module.css'
-import ExpenseForm from './components/ExpenseForm'
+import styles from './styles/style.module.css'
+import appStyles from './App.module.css'
+import ExpenseForm from './components/ExpenseForm.jsx'
 
 function App() {
 
   return (
     <>
-    <ExpenseForm />
+    <div className={`${appStyles.app_container} ${styles.flex} ${styles.flex_column}`}>
+      <nav className={`${appStyles.navbar} ${styles.flex}`}>
+        <span className={`${appStyles.nav_header}`}>Expense Tracker</span>
+      </nav>
+
+      <div className={`${styles.flex} ${styles.flex_row}`}>
+        <div className={`${appStyles.category_container} ${styles.flex} ${styles.flex_column}`}>
+          <h2>Total category expenses</h2>
+        </div>
+      </div>
+
+      <ExpenseForm />
+    </div>
+    
       
     </>
   )
