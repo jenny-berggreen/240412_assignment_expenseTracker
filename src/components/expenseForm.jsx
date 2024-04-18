456fed556d3356ae75f2499e04e76634814d01d7
jenny-berggreen <jenny@berggreen.no>
2024-04-18 10:46:43 +0200

    Style expense form

--- src/components/expenseForm.jsx
+++ src/components/expenseForm.jsx
@@ -9,28 +9,28 @@ const ExpenseForm = () => {
     <form className={`${formStyles.expense_form} ${styles.flex}`}>
       <fieldset className={`${formStyles.expense_form_container} ${styles.flex} ${styles.flex_column}`}>
 
-        <section className={`${styles.flex} ${styles.flex_row}`}>
-          <div className={`${formStyles.input_group} ${styles.flex} ${styles.flex_column}`}>
+        <section className={`${formStyles.form_section} ${styles.flex} ${styles.flex_row}`}>
+          <div className={`${formStyles.input_group} ${formStyles.input_group__title} ${styles.flex} ${styles.flex_column}`}>
             <label htmlFor="title">Title<sup>*</sup></label>
 						<input type="text" name='title' placeholder='Ex. bus ticket' className={`${formStyles.input_element}`} />
 						<p>Error</p>
           </div>
-          <div className={`${formStyles.input_group} ${styles.flex} ${styles.flex_column}`}>
+          <div className={`${formStyles.input_group} ${formStyles.input_group__amount} ${styles.flex} ${styles.flex_column}`}>
             <label htmlFor="amount">Amount<sup>*</sup></label>
-						<input type="text" name='amount' placeholder='Ex. 3.50' className={`${formStyles.input_element} ${formStyles.input_element__amount}`} />
+						<input type="text" name='amount' placeholder='Ex. 3.50' className={`${formStyles.input_element}`} />
 						<p>Error</p>
           </div>
         </section>
 
-        <div className={`${formStyles.input_group} ${styles.flex} ${styles.flex_column}`}>
+        <div className={`${formStyles.input_group} ${formStyles.input_group__date} ${styles.flex} ${styles.flex_column}`}>
             <label htmlFor="date">Date<sup>*</sup></label>
 						<input type="date" name='date' className={`${formStyles.input_element}`} />
 						<p>Error</p>
         </div>
 
-        <div className={`${formStyles.input_group} ${styles.flex} ${styles.flex_column}`}>
-            <label htmlFor="category">Category<sup>*</sup></label>
-						<select name="category">
+        <div className={`${formStyles.input_group} ${formStyles.input_group__category} ${styles.flex} ${styles.flex_column}`}>
+            <label htmlFor="category">Category</label>
+						<select name="category" className={`${formStyles.input_element}`}>
               <option value="noCategory">No category</option>
               <option value="housing">Housing</option>
               <option value="groceries">Groceries</option>
