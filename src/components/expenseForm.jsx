b9961a4c6943307a51dabc6dc4fc8a67a5badf63
jenny-berggreen <jenny@berggreen.no>
2024-04-19 11:18:48 +0200

    Reset form after submission

--- src/components/expenseForm.jsx
+++ src/components/expenseForm.jsx
@@ -86,25 +86,25 @@ const ExpenseForm = ({ onAddExpense, setIsValid }) => {
         <section className={`${formStyles.form_section} ${styles.flex} ${styles.flex_row}`}>
           <div className={`${formStyles.input_group} ${formStyles.input_group__title} ${styles.flex} ${styles.flex_column}`}>
             <label htmlFor="title">Title<sup>*</sup></label>
-						<input type="text" name='title' placeholder='Ex. bus ticket' className={`${formStyles.input_element}`} onChange={handleChange} />
+						<input type="text" name='title' placeholder='Ex. bus ticket' className={`${formStyles.input_element}`} value={expenseData.title} onChange={handleChange} />
 						<p>{errors.titleError}</p>
           </div>
           <div className={`${formStyles.input_group} ${formStyles.input_group__amount} ${styles.flex} ${styles.flex_column}`}>
             <label htmlFor="amount">Amount<sup>*</sup></label>
-						<input type="number" name='amount' placeholder='Ex. 3.50' className={`${formStyles.input_element}`} onChange={handleChange} />
+						<input type="number" name='amount' placeholder='Ex. 3.50' className={`${formStyles.input_element}`} value={expenseData.amount} onChange={handleChange} />
 						<p>{errors.amountError}</p>
           </div>
         </section>
 
         <div className={`${formStyles.input_group} ${formStyles.input_group__date} ${styles.flex} ${styles.flex_column}`}>
             <label htmlFor="date">Date<sup>*</sup></label>
-						<input type="date" name='date' className={`${formStyles.input_element}`} onChange={handleChange} />
+						<input type="date" name='date' className={`${formStyles.input_element}`} value={expenseData.date} onChange={handleChange} />
 						<p>{errors.dateError}</p>
         </div>
 
         <div className={`${formStyles.input_group} ${formStyles.input_group__category} ${styles.flex} ${styles.flex_column}`}>
             <label htmlFor="category">Category</label>
-						<select name="category" className={`${formStyles.input_element}`} onChange={handleChange}>
+						<select name="category" className={`${formStyles.input_element}`} value={expenseData.category} onChange={handleChange}>
               <option value="">No category</option>
               <option value="housing">Housing</option>
               <option value="groceries">Groceries</option>
