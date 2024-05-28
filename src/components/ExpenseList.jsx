5438c0bdcc5155b33781b0e7101f35a15d3cb6ae
jenny-berggreen <jenny@berggreen.no>
2024-05-28 14:04:55 +0200

    Remove comment

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -24,21 +24,3 @@ const ExpenseList = ({ expenses, onDeleteExpense }) => {
 }
 
 export default ExpenseList
-
-{/* <div className={`${listStyles.expense_list_container}`}>
-		<span className={`${listStyles.expense_list_row} ${styles.flex} ${styles.flex_row}`}>
-			<div className={`${listStyles.title_date_container} ${listStyles.list_row_item__title_date}`}>
-				<span>dd/mm/yy</span>
-				<span>Rent April</span>
-			</div>
-			<div className={`${listStyles.category_container} ${listStyles.list_row_item__category} ${styles.flex}`}>
-				<span>Housing</span>
-			</div>
-			<div className={`${listStyles.amount_container} ${listStyles.list_row_item__amount} ${styles.flex}`}>
-				<span>$3.50</span>
-			</div>
-			<div className={`${listStyles.button_container} ${listStyles.list_row_item__button} ${styles.flex}`}>
-				<button className={`${listStyles.delete_button}`}>Delete</button>
-			</div>
-		</span>
-	</div> */}
